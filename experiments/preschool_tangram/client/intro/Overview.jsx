5ee39de72b4f8d9d5715304ed4c85fb57e5dc740
Veronica Boyce <vero.boyce@gmail.com>
2023-07-03 13:35:54 -0700

    ios is stupid about sound

--- experiments/preschool_tangram/client/intro/Overview.jsx
+++ experiments/preschool_tangram/client/intro/Overview.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import { Centered } from "meteor/empirica:core";
 import { Button } from "@blueprintjs/core";
+const roundSound = new Audio("experiment/round-sound.mp3");
 
 export default class Overview extends React.Component {
   render() {
     const { hasPrev, hasNext, onNext, onPrev, treatment } = this.props;
+    function finish(){
+      roundSound.play()
+      onNext
+    }
     return (
       <Centered>
         <div className="instructions">
@@ -13,7 +18,7 @@ export default class Overview extends React.Component {
           <button
             type="button"
             className="bp3-button bp3-intent-primary"
-            onClick={onNext}
+            onClick={finish}
           >
             
             <span className="bp3-icon-standard bp3-icon-double-chevron-right bp3-align-right"/>
